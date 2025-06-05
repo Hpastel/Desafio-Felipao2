edc7678d527cc275a65a4594cba1e9cb718e6801
hermesAlves <hermesalvesporto@gmail.com>
2025-06-05 17:52:32 +0100

    primeiro commit desafio 2

---
+++ src/index.js
@@ -0,0 +1,44 @@
+let vitorias = 105;
+let derrotas = 20;
+let ranking = vitorias - derrotas;
+let nivel = "";
+    
+// ["Ferro, Bronze, Prata, Ouro, Diamante, Lendário, Imortal"];
+
+
+
+if (vitorias - derrotas < 10) {
+    console.log("Ranking Ferro")
+    nivel = "Ferro";
+}
+else if (vitorias - derrotas < 21 && vitorias - derrotas > 10) {
+    console.log("Ranking Bronze")
+    nivel = "Bronze";
+}
+else if (vitorias - derrotas < 51 && vitorias - derrotas > 20) {
+    console.log("Ranking Prata")
+    nivel = "Prata";
+}
+else if (vitorias - derrotas < 81 && vitorias - derrotas > 50) {
+    console.log("Ranking Ouro")
+    nivel = "Ouro";
+}
+else if (vitorias - derrotas < 91 && vitorias - derrotas > 80) {
+    console.log("Ranking Diamante")
+    nivel = "Diamante";
+}
+else if (vitorias - derrotas < 101 && vitorias - derrotas > 90) {
+    console.log("Ranking Lendário")
+    nivel = "Lendário";
+}
+else if (vitorias - derrotas >= 101) {
+    console.log("Ranking Imortal")
+    nivel = "Imortal";
+}
+else {
+    console.log("Erro ao calcular o ranking")
+    
+}
+
+console.log("O Herói tem saldo de " + ranking + " vitórias e esta no nível " + nivel + " do ranking.");
+
---
+++ src/testes.js
@@ -0,0 +1,80 @@
+
+// saldoFinal = parseInt(gets())
+// solucao DESAFIO 2, encontrada em post na DIO, testar!!
+
+// let material = { "Ferro":"Bronze", "Prata": "Ouro", "Diamante": "Lendário", "Imortal": "Mítico" } 
+// let inicio = {0: 11, 21: 51, 81: 91}
+// let fim = {10: 20, 50: 80, 90: 100}
+// let nivel = material.length - 1
+
+// for (let i = 0; i < nivel; i++) {
+//   if (saldoFinal >= inicio[i] && saldoFinal <= fim[i]) {
+//     nivel = i
+//     break 
+//   }
+// }
+ 
+// console.log(material[nivel])
+
+
+// function calcularIdade(nome, anoNascimento) {
+//   let idade = 2023 - anoNascimento;
+//   console.log(nome + " tem " + idade + " anos!");
+// }
+
+// calcularIdade("Pedrinho", 2010); // Vai mostrar Pedrinho tem 13 anos!
+// calcularIdade("Mariana", 2008); // Vai mostrar Mariana tem 15 anos!
+
+
+// enviarDados();
+
+// function enviarDados() { 
+//     let nomeDoBanco = "banco-de-dados";
+//     console.log("salvando dados em :" + nomeDoBanco);
+
+//     if (3 === 3) {
+//         console.log("senha valida");
+//     }
+// }
+
+
+// mainSaveData();
+
+// function mainSaveData() {
+//     getData();
+//     sendToDatabase();
+//     checkValues();
+
+// }
+
+// // função que pega todos os dados
+// function getData() {
+//     console.log("Pegou os dados do utilizador");
+//     if (1 < 3) {
+//         console.log("numero encontrado");
+//     }
+// }
+
+// // lógica para enviar dados para o banco de dados
+// function sendToDatabase() {
+//     console.log("enviei os dados para a BD");
+// }
+
+// function checkValues() {
+//     console.log("verificando valores");
+    
+// }
+
+
+// torrar();
+
+// function torrar() {
+//     console.log("torrando pão");
+//     injetarPao();
+// }
+
+// function injetarPao() {
+//     console.log("Preparando para injetar pão");
+//     console.log("Passar manteiga no pão");
+//     console.log("finalizado o pão");
+// }
