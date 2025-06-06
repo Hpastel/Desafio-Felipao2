7e72eb18c15f0655c40174dcb5983ad8d8a8e058
hermesAlves <hermesalvesporto@gmail.com>
2025-06-06 09:55:47 +0100

    inserção de sugestão/melhoramento de solução do desafio e renomeamento do file 'testes' para 'testesESugestoes'

--- src/testesESugestoes.js
+++ src/testesESugestoes.js
@@ -1,3 +1,33 @@
+//Sugestão de solução-melhoramento vinda do DEEPSEEK após a entrega do desafio:
+
+let vitorias = 105;
+let derrotas = 20;
+let saldoVitorias = vitorias - derrotas;
+let nivel;
+
+if (saldoVitorias < 10) {
+    nivel = "Ferro";
+} 
+else if (saldoVitorias <= 20) {
+    nivel = "Bronze";
+} 
+else if (saldoVitorias <= 50) {
+    nivel = "Prata";
+} 
+else if (saldoVitorias <= 80) {
+    nivel = "Ouro";
+} 
+else if (saldoVitorias <= 90) {
+    nivel = "Diamante";
+} 
+else if (saldoVitorias <= 100) {
+    nivel = "Lendário";
+} 
+else {
+    nivel = "Imortal";
+}
+
+console.log(`O Herói tem saldo de ${saldoVitorias} vitórias e está no nível ${nivel} do ranking.`);
 
 // saldoFinal = parseInt(gets())
 // solucao DESAFIO 2, encontrada em post na DIO, testar!!
